33da137b363c6231a95bf33fa2c3e09a973808d0
Paresh <pareshvarde@outlook.com>
2018-01-03 16:04:51 +0530

    add owin start up configured bearer token generation

---
+++ BK/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+
+[assembly: OwinStartup(typeof(BK.Startup))]
+namespace BK
+{    
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            ConfigureOAuth(app);
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new bkAuthorizationServerProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
---
+++ BK/bkAuthorizationServerProvider.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using BK.Context;
+
+namespace BK
+{
+    public class bkAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+            familymember fMember = null;
+
+            using (bkContext _context = new bkContext())
+            {
+                fMember = _context.familymembers.FirstOrDefault(m => m.EmailAddress == context.UserName);
+                if (fMember != null)
+                {
+                    login fLogin = _context.logins.FirstOrDefault(l => l.Password == context.Password && l.Active == true);
+                    if (fLogin == null)
+                    {
+                        context.SetError("invalid_grant", "The user name or password is incorrect.");
+                        return;
+                    }
+                }
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+            identity.AddClaim(new Claim("familyId", fMember.FamilyID.ToString()));
+            identity.AddClaim(new Claim("familyMemberId", fMember.FamilyMemberID.ToString()));
+
+            context.Validated(identity);
+        }
+    }
+}
