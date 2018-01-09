1e0805cd9977722bd41364803de2c413891c4d33
Paresh <pareshvarde@outlook.com>
2018-01-09 18:03:27 +0530

    changed navigation color

--- BK/bk-ui/src/app/core/services/config.service.ts
+++ BK/bk-ui/src/app/core/services/config.service.ts
@@ -29,9 +29,9 @@ export class FuseConfigService
                 mode            : 'fullwidth' // 'boxed', 'fullwidth'
             },
             colorClasses    : {
-                toolbar: 'mat-white-500-bg',
-                navbar : 'mat-fuse-dark-700-bg',
-                footer : 'mat-fuse-dark-900-bg'
+                toolbar: 'mat-orange-500-bg',
+                navbar : 'mat-orange-700-bg',
+                footer : 'mat-orange-900-bg'
             },
             customScrollbars: true,
             routerAnimation : 'fadeIn' // fadeIn, slideUp, slideDown, slideRight, slideLeft
