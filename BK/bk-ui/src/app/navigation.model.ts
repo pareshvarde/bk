5dd1bcd82b695f0816651067719c3a6807947a11
Paresh <pareshvarde@outlook.com>
2018-01-09 16:07:08 +0530

    added menu

--- BK/bk-ui/src/app/navigation.model.ts
+++ BK/bk-ui/src/app/navigation.model.ts
@@ -6,24 +6,90 @@ export class NavigationModel
     {
         this.model = [
             {
-                'id'      : 'applications',
-                'title'   : 'Applications',
-                'type'    : 'group',
+                'id'      : 'portal',
+                'title'   : 'Portal',
+                'type'    : 'collapse',
+                'icon'    : '',
                 'children': [
                     {
-                        'id'   : 'sample',
-                        'title': 'Sample',
+                        'id'   : 'origin',
+                        'title': 'Origin',
                         'type' : 'item',
-                        'icon' : 'email',
-                        'url'  : '/sample',
-                        'badge': {
-                            'title': 25,
-                            'bg'   : '#F44336',
-                            'fg'   : '#FFFFFF'
-                        }
-                    }
+                        'url'  : '/pages/auth/login'
+                    },                                                  
+                    {
+                        'id'   : 'nukh',
+                        'title': 'Nukh',
+                        'type' : 'item',
+                        'url'  : '/pages/auth/mail-confirm'
+                    },
+                    {
+                        'id'   : 'gotras',
+                        'title': 'Gotra',
+                        'type' : 'item',
+                        'url'  : '/pages/auth/mail-confirm'
+                    },
+                    {
+                        'id'   : 'temples',
+                        'title': 'Temples',
+                        'type' : 'item',
+                        'url'  : '/pages/auth/mail-confirm'
+                    },
+                    {
+                        'id'   : 'hostels',
+                        'title': 'Hostels',
+                        'type' : 'item',
+                        'url'  : '/pages/auth/mail-confirm'
+                    },
+                    {
+                        'id'   : 'organizations',
+                        'title': 'Organizations',
+                        'type' : 'item',
+                        'url'  : '/pages/auth/mail-confirm'
+                    }                    
+                ]
+            },
+            {
+                'id'      : 'directory',
+                'title'   : 'Directory',
+                'type'    : 'collapse',
+                'icon'    : '',
+                'children': [
+                    {
+                        'id'   : 'searchDirectory',
+                        'title': 'Search Directory',
+                        'type' : 'item',
+                        'url'  : '/pages/auth/login'
+                    },                                                  
+                    {
+                        'id'   : 'myAccount',
+                        'title': 'My Family',
+                        'type' : 'item',
+                        'url'  : '/pages/auth/mail-confirm'
+                    }                   
+                ]
+            },
+            {
+                'id'      : 'matrimony',
+                'title'   : 'Matrimony',
+                'type'    : 'collapse',
+                'icon'    : '',
+                'children': [
+                    {
+                        'id'   : 'searchMatrimony',
+                        'title': 'Search Matrimony',
+                        'type' : 'item',
+                        'url'  : '/pages/auth/login'
+                    },                                                  
+                    {
+                        'id'   : 'myProfile',
+                        'title': 'My Profile',
+                        'type' : 'item',
+                        'url'  : '/pages/auth/mail-confirm'
+                    }                   
                 ]
             }
         ];
     }
 }
+
