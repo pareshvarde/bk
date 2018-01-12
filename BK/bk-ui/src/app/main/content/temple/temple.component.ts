edaa4d5d45748ff542f7c8fade748e30e00b4d90
Paresh <pareshvarde@outlook.com>
2018-01-12 15:53:28 +0530

    added temple information

--- BK/bk-ui/src/app/main/content/temple/temple.component.ts
+++ BK/bk-ui/src/app/main/content/temple/temple.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-temple',
@@ -9,7 +10,125 @@ export class TempleComponent implements OnInit {
 
   constructor() { }
 
+  displayedColumns = ['mainNukh', 'subNukh1', 'subNukh2', 'subNukh3', 'subNukh4',
+  'subNukh5', 'subNukh6', 'subNukh7', 'subNukh8',
+  'subNukh9', 'subNukh10', 'subNukh11', 'subNukh12'];
+
+  dataSource = new MatTableDataSource<TempleModel>(TEMPLE_DATA);
+
   ngOnInit() {
   }
 
 }
+
+export interface TempleModel {
+  name: string;
+  image: string,
+  latitude: string;
+  longitude: string;
+  address: string;
+  city: string,  
+  state: string,
+  country: string
+  contactName: string;
+  phoneNumber: string;  
+}
+
+const TEMPLE_DATA: TempleModel[] = [
+  {    
+    name: "Hinglaj Mandir, Daheli",
+    image: "noImage.jpg",
+    latitude: "121.445454",
+    longitude: "-80.454541",
+    address: "Samaj Bhavan",
+    city: "Daheli",
+    state: "Gujarat",
+    country: "India",
+    contactName: "Paresh Varde",
+    phoneNumber: "9970339703"    
+  },
+  {    
+    name: "Hinglaj Mandir, Daheli",
+    image: "noImage.jpg",
+    latitude: "121.445454",
+    longitude: "-80.454541",
+    address: "Samaj Bhavan",
+    city: "Daheli",
+    state: "Gujarat",
+    country: "India",
+    contactName: "Paresh Varde",
+    phoneNumber: "9970339703"    
+  },
+  {    
+    name: "Hinglaj Mandir, Daheli",
+    image: "noImage.jpg",
+    latitude: "121.445454",
+    longitude: "-80.454541",
+    address: "Samaj Bhavan",
+    city: "Daheli",
+    state: "Gujarat",
+    country: "India",
+    contactName: "Paresh Varde",
+    phoneNumber: "9970339703"    
+  },
+  {    
+    name: "Hinglaj Mandir, Daheli",
+    image: "noImage.jpg",
+    latitude: "121.445454",
+    longitude: "-80.454541",
+    address: "Samaj Bhavan",
+    city: "Daheli",
+    state: "Gujarat",
+    country: "India",
+    contactName: "Paresh Varde",
+    phoneNumber: "9970339703"    
+  },
+  {    
+    name: "Hinglaj Mandir, Daheli",
+    image: "noImage.jpg",
+    latitude: "121.445454",
+    longitude: "-80.454541",
+    address: "Samaj Bhavan",
+    city: "Daheli",
+    state: "Gujarat",
+    country: "India",
+    contactName: "Paresh Varde",
+    phoneNumber: "9970339703"    
+  },
+  {    
+    name: "Hinglaj Mandir, Daheli",
+    image: "noImage.jpg",
+    latitude: "121.445454",
+    longitude: "-80.454541",
+    address: "Samaj Bhavan",
+    city: "Daheli",
+    state: "Gujarat",
+    country: "India",
+    contactName: "Paresh Varde",
+    phoneNumber: "9970339703"    
+  },
+  {    
+    name: "Hinglaj Mandir, Daheli",
+    image: "noImage.jpg",
+    latitude: "121.445454",
+    longitude: "-80.454541",
+    address: "Samaj Bhavan",
+    city: "Daheli",
+    state: "Gujarat",
+    country: "India",
+    contactName: "Paresh Varde",
+    phoneNumber: "9970339703"    
+  },
+  {    
+    name: "Hinglaj Mandir, Daheli",
+    image: "noImage.jpg",
+    latitude: "121.445454",
+    longitude: "-80.454541",
+    address: "Samaj Bhavan",
+    city: "Daheli",
+    state: "Gujarat",
+    country: "India",
+    contactName: "Paresh Varde",
+    phoneNumber: "9970339703"    
+  },
+];
