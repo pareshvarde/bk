b25e6fca5cf5482b49f8d501304f7b3c14806f41
Paresh <pareshvarde@outlook.com>
2018-01-12 17:10:00 +0530

    no message

--- BK/bk-ui/src/app/main/content/temple/temple.component.ts
+++ BK/bk-ui/src/app/main/content/temple/temple.component.ts
@@ -36,16 +36,40 @@ export interface TempleModel {
 
 const TEMPLE_DATA: TempleModel[] = [
   {    
-    name: "Hinglaj Mandir, Daheli",
-    image: "noImage.jpg",
-    latitude: "121.445454",
-    longitude: "-80.454541",
-    address: "Samaj Bhavan",
-    city: "Daheli",
-    state: "Gujarat",
+    name: "Shakti Peeth Shri Hinglaj Mata",
+    image: "baluchistan.jpg",
+    latitude: "25.509673",
+    longitude: "65.5118986",
+    address: "Hingol Park Road, Pakistan",
+    city: "Lyari Tehsil",
+    state: "Balochistan",
+    country: "Pakistan",
+    contactName: "<NOT AVAILABLE>",
+    phoneNumber: "<NOT AVAILABLE>"
+  },
+  {    
+    name: "Hinglaj Mata Temple",
+    image: "barmer.jpg",
+    latitude: "25.7579066",
+    longitude: "71.3833546",
+    address: "Indra Nagar",
+    city: "Barmer",
+    state: "Rajasthan",
     country: "India",
-    contactName: "Paresh Varde",
-    phoneNumber: "9970339703"    
+    contactName: "<NOT AVAILABLE>",
+    phoneNumber: "<NOT AVAILABLE>"
+  },
+  {    
+    name: "Hinglaj Mata Mandir",
+    image: "bhagli.jpg",
+    latitude: "25.1260852",
+    longitude: "73.2166689",
+    address: "Bhagli",
+    city: "Bhagli",
+    state: "Rajasthan",
+    country: "India",
+    contactName: "<NOT AVAILABLE>",
+    phoneNumber: "<NOT AVAILABLE>"    
   },
   {    
     name: "Hinglaj Mandir, Daheli",
@@ -131,4 +155,28 @@ const TEMPLE_DATA: TempleModel[] = [
     contactName: "Paresh Varde",
     phoneNumber: "9970339703"    
   },
+  {    
+    name: "Hinglaj Mandir, Daheli",
+    image: "noImage.jpg",
+    latitude: "121.445454",
+    longitude: "-80.454541",
+    address: "Samaj Bhavan",
+    city: "Daheli",
+    state: "Gujarat",
+    country: "India",
+    contactName: "Paresh Varde",
+    phoneNumber: "9970339703"    
+  },
+  {    
+    name: "Hinglaj Mandir, Daheli",
+    image: "noImage.jpg",
+    latitude: "121.445454",
+    longitude: "-80.454541",
+    address: "Samaj Bhavan",
+    city: "Daheli",
+    state: "Gujarat",
+    country: "India",
+    contactName: "Paresh Varde",
+    phoneNumber: "9970339703"    
+  }
 ];
