e773c4b37e1a1fe2d25a54b98860b48eb35817c8
Paresh <pareshvarde@outlook.com>
2018-01-15 19:02:10 +0530

    no message

--- BK/bk-ui/src/app/main/content/login/login.component.ts
+++ BK/bk-ui/src/app/main/content/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { bkDataService } from '../../services/bk-data.service';
+import { Router } from '@angular/router';
+import { Response } from '@angular/http/src/static_response';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +15,7 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   loginFormErrors: any;  
 
-  constructor(private formBuilder: FormBuilder, private dataService: bkDataService) { 
+  constructor(private formBuilder: FormBuilder, private router: Router, private dataService: bkDataService) { 
     this.loginFormErrors = {
       email   : {},
       password: {}
@@ -51,6 +53,19 @@ export class LoginComponent implements OnInit {
   }
 
   processLogin(){
-    let token = this.dataService.login(this.loginForm.controls.email.value, this.loginForm.controls.password.value)
+    let localStorage = window.localStorage;
+    localStorage.removeItem('token');
+
+    this.dataService.login(this.loginForm.controls.email.value, this.loginForm.controls.password.value).subscribe(
+      (res) =>{      
+        let result = JSON.parse((<any>res)._body)        
+        localStorage.setItem('token', result.access_token);
+        this.router.navigate(['home']);
+      },
+      (err) =>{        
+        let error = JSON.parse((<any>err)._body)
+        console.log(err);
+      }
+    );    
   }
 }
--- BK/bk-ui/src/app/main/services/bk-data.service.ts
+++ BK/bk-ui/src/app/main/services/bk-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { HttpErrorResponse } from '@angular/common/http/src/response';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class bkDataService {
@@ -8,25 +9,13 @@ export class bkDataService {
 
   constructor(private http: Http) { }
 
-  login(userName: string, password: string): any {
-    this.http.post(this.API_URL + "login", "username=" + userName + "&password=" + password + "&grant_type=password").subscribe(response => {
-
-      debugger;
-
-      let result = JSON.parse(response._body);
-
-      if (response.status === 200) {
-        let localStorage = window.localStorage;
-        localStorage.setItem('token', result.access_token);
-      }
-      else {
-
-      }
+  login(userName: string, password: string): Observable<any> {
+    return this.http.post(this.API_URL + "login", "username=" + userName + "&password=" + password + "&grant_type=password").map(response => {
+      return response;
     },
-    (err: HttpErrorResponse) =>{
-      debugger;      
-      let error = JSON.parse(err._body).error_description;
-    }
-  );
+      (err: HttpErrorResponse) => {
+        return err;
+      }
+    );
   };
 }
