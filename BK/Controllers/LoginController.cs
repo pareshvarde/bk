e33f144f327746b2f4d608a59fb2daf35923da6f
Paresh <pareshvarde@outlook.com>
2018-01-15 16:17:09 +0530

    updated VS project

--- BK/Controllers/LoginController.cs
+++ BK/Controllers/LoginController.cs
@@ -1,5 +1,4 @@
-﻿using BK.Context;
-using BK.Utility;
+﻿using BK.Utility;
 using BK.ViewModel;
 using System;
 using System.Collections.Generic;
@@ -16,48 +15,36 @@ namespace BK.Controllers
         [HttpPost]
         public IHttpActionResult Register(Register register)
         {
-            using (bkContext context = new bkContext())
-            {
-                if (context.familymembers.Any(f => f.EmailAddress == register.EmailAddress.Trim()))
-                    return BadRequest("Email address already registered");
+            //using (bkContext context = new bkContext())
+            //{
+            //    if (context.familymembers.Any(f => f.EmailAddress == register.EmailAddress.Trim()))
+            //        return BadRequest("Email address already registered");
 
-                List<login> logins = new List<login>();
-                logins.Add(new login()
-                {
-                    Active = true,
-                    Password = register.Password
-                });
+            //    List<login> logins = new List<login>();
+            //    logins.Add(new login()
+            //    {
+            //        Active = true,
+            //        Password = register.Password
+            //    });
 
-                family fam = new family();
-                fam.FamilyNumber = IDGenerator.CreateSID(IDGenerator.Prefixes.FAMILY);
+            //    family fam = new family();
+            //    fam.FamilyNumber = IDGenerator.CreateSID(IDGenerator.Prefixes.FAMILY);
 
-                fam.familymembers.Add(new familymember()
-                {
-                    EmailAddress = register.EmailAddress,
-                    FirstName = register.FirstName,
-                    LastName = register.LastName,
-                    Gender = register.Male,
-                    logins = logins
-                });
+            //    fam.familymembers.Add(new familymember()
+            //    {
+            //        EmailAddress = register.EmailAddress,
+            //        FirstName = register.FirstName,
+            //        LastName = register.LastName,
+            //        Gender = register.Male,
+            //        logins = logins
+            //    });
 
-                context.families.Add(fam);
+            //    context.families.Add(fam);
 
-                context.SaveChanges();
+            //    context.SaveChanges();
 
                 return Ok();
             }
-        }
-
-        [Route("api/verifyEmailaddress")]
-        public IHttpActionResult EmailAddressAvailable(string email)
-        {
-            using (bkContext context = new bkContext())
-            {
-                if (context.familymembers.Any(f => f.EmailAddress == email.Trim()))
-                    return Ok(false);
-                else
-                    return Ok(true);
-            }
-        }
+        }       
     }
 }
