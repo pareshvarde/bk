59f562727af3134039c408ca3c62f4db9584c8f5
Paresh <pareshvarde@outlook.com>
2018-01-18 17:04:37 +0530

    no message

--- BK/bk-ui/src/app/main/content/change-password/change-password.component.ts
+++ BK/bk-ui/src/app/main/content/change-password/change-password.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { bkDataService } from '../../services/bk-data.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Response } from '@angular/http/src/static_response';
+import { NotificationsService } from 'angular2-notifications';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.scss']
+  styleUrls: ['./change-password.component.scss'],
+  providers: [bkDataService]
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  changePasswordForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder, private dataService: bkDataService, private alertService: NotificationsService) { 
+
   }
 
+  ngOnInit() {
+    this.changePasswordForm = this.formBuilder.group({
+      currentPassword: new FormControl('', Validators.required),
+      newPassword: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required)      
+    });    
+  }
+
+  changePassword(){
+
+  }
 }
