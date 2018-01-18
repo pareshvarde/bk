c4262ac363c53243f93fd8e5d9e7090c06c8bedf
Paresh <pareshvarde@outlook.com>
2018-01-18 13:51:54 +0530

    no message

--- BK/bk-ui/src/app/main/services/bk-data.service.ts
+++ BK/bk-ui/src/app/main/services/bk-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { HttpErrorResponse } from '@angular/common/http/src/response';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/catch';
 import { error } from 'selenium-webdriver';
 
