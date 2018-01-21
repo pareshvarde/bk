64d2e4c9ca5be78cb8d878b6d9b6fb568d6672d5
Paresh Varde <pareshvarde@yahoo.com>
2018-01-21 18:24:36 +0530

    no message

--- BK/ViewModel/RegisterViewModel.cs
+++ BK/ViewModel/RegisterViewModel.cs
@@ -16,12 +16,27 @@ namespace BK.ViewModel
         public string FirstName { get; set; }
         [JsonProperty("lastName")]
         public string LastName { get; set; }
-        [JsonProperty("emailAddress")]
+        [JsonProperty("email")]
         public string EmailAddress { get; set; }
-        [JsonProperty("password")]
-        public string Password { get; set; }
-        [JsonProperty("male")]
-        public bool Male { get; set; }
+        [JsonProperty("phoneNumber")]
+        public string PhoneNumber { get; set; }
+        [JsonProperty("aadhaarNumber")]
+        public string AadhaarNumber { get; set; }
+        [JsonProperty("gender")]
+        public bool Gender { get; set; }
+        [JsonProperty("dob")]
+        public DateTime DateOfBirth { get; set; }
+        [JsonProperty("address1")]
+        public string Address1 { get; set; }
+        [JsonProperty("address2")]
+        public string Address2 { get; set; }
+        [JsonProperty("city")]
+        public string City { get; set; }
+        [JsonProperty("state")]
+        public string State { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
     }
 
     public class RegisterValidatorViewModel : AbstractValidator<RegisterViewModel>
@@ -30,8 +45,7 @@ namespace BK.ViewModel
         {
             RuleFor(x => x.FirstName).NotEmpty().WithMessage("First Name cannot be blank");
             RuleFor(x => x.LastName).NotEmpty().WithMessage("Last Name cannot be blank");
-            RuleFor(x => x.EmailAddress).NotEmpty().WithMessage("Email address cannot be blank").EmailAddress().WithMessage("Invalid email address");
-            RuleFor(x => x.Password).NotNull().WithMessage("Password cannot be blank");
+            RuleFor(x => x.EmailAddress).NotEmpty().WithMessage("Email address cannot be blank").EmailAddress().WithMessage("Invalid email address");            
         }
     }
 }
--- BK/bk-ui/src/app/main/content/register/register.component.ts
+++ BK/bk-ui/src/app/main/content/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl,Validators } from '@angular/forms';
+import { EmailValidators, UniversalValidators } from 'ng2-validators';
 
 @Component({
   selector: 'app-register',
@@ -9,24 +10,45 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;  
+  formModel: RegisterViewModel;
   
-  constructor() { }
+  constructor() { 
+    this.formModel = new RegisterViewModel();
+  }
 
   ngOnInit() {
     this.registerForm = new FormGroup({      
-      firstName: new FormControl('', null),
-      lastName: new FormControl('', null),
-      email: new FormControl('', null),
-      phoneNumber: new FormControl('', null),
-      aadhaarNumber: new FormControl('', null),
-      gender: new FormControl('', null),
-      dob: new FormControl('', null),
+      firstName: new FormControl('', [UniversalValidators.noWhitespace,Validators.required]),
+      lastName: new FormControl('', [UniversalValidators.noWhitespace,Validators.required]),
+      email: new FormControl('', [EmailValidators.normal,Validators.required]),
+      phoneNumber: new FormControl('', [UniversalValidators.isNumber]),
+      aadhaarNumber: new FormControl('', [UniversalValidators.isNumber]),
+      gender: new FormControl(1, null),
+      dob: new FormControl('', [Validators.required]),
       address1: new FormControl('', null),
       address2: new FormControl('', null),
-      city: new FormControl('', null),
-      state: new FormControl('', null),
-      country: new FormControl('', null),
+      city: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required]),
+      country: new FormControl('India', [Validators.required]),
     }); 
   }
 
+  processRegistration(){
+
+  }
+}
+
+export class RegisterViewModel{
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: number;
+  aadhaarNumber: number;
+  gender: boolean;
+  dob: Date;
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  country: string;
 }
