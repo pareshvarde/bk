5dbe8997a104fc81e6d9a0dd8a399830f86161fe
Paresh Varde <pareshvarde@yahoo.com>
2018-01-21 10:43:35 +0530

    no message

--- BK/bk-ui/src/app/main/services/bk-data.service.ts
+++ BK/bk-ui/src/app/main/services/bk-data.service.ts
@@ -6,24 +6,28 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import { error } from 'selenium-webdriver';
 import { changePasswordViewModel } from '../content/change-password/change-password.component';
+import { NgBlockUI, BlockUI } from 'ng-block-ui';
 
 @Injectable()
 export class bkDataService {
   private API_URL = "http://localhost:60067/api/";
+  @BlockUI() blockUI: NgBlockUI;
 
   constructor(private http: Http) { }
 
   login(userName: string, password: string): Observable<any> {
+    debugger;
+    this.blockUI.start("Please wait...");
+
     return this.http.post(this.API_URL + "login", "username=" + userName + "&password=" + password + "&grant_type=password").map(response => {
+      debugger;
+      this.blockUI.stop();
       return response;
-    },
-      (err: HttpErrorResponse) => {
-        return err;
-      }
-    );
+    }).catch((error: any) => this.handleAPIError(error));      
   };
 
   sendResetPasswordEmail(emailAddress: string) {
+
     return this.http.get(this.API_URL + "sendResetPasswordEmail?emailAddress=" + emailAddress).map((res) => {
       return this.handleAPIResponse(res);
     }).catch((error: any) => this.handleAPIError(error));
