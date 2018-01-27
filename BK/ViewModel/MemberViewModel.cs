c971dca358765311ff8aa030c632df1d8dc0dba4
Paresh Varde <pareshvarde@yahoo.com>
2018-01-27 12:15:59 +0530

    no message

--- BK/ViewModel/MemberViewModel.cs
+++ BK/ViewModel/MemberViewModel.cs
@@ -13,44 +13,64 @@ namespace BK.ViewModel
     {
         [JsonProperty("memberId")]
         public int MemberID { get; set; }
+
         [JsonProperty("firstName")]
         public string FirstName { get; set; }
+
         [JsonProperty("lastName")]
         public string LastName { get; set; }
+
         [JsonProperty("nickName")]
         public string NickName { get; set; }
+
         [JsonProperty("email")]
         public string Email { get; set; }
+
         [JsonProperty("phoneNumber")]
         public string PhoneNumber { get; set; }
+
         [JsonProperty("aadhaarNumber")]
         public long? AadhaarNumber { get; set; }
+
         [JsonProperty("gender")]
         public string Gender { get; set; }
+
         [JsonProperty("dob")]
         public DateTime? DOB { get; set; }
+
         [JsonProperty("birthPlace")]
         public string BirthPlace { get; set; }
+
         [JsonProperty("alive")]
         public string Alive { get; set; }
+
         [JsonProperty("dod")]
         public DateTime? DOD { get; set; }
+
         [JsonProperty("deathPlace")]
         public string DeathPlace { get; set; }
+
         [JsonProperty("married")]
         public bool Married { get; set; }
+
         [JsonProperty("educationLevel")]
         public string EducationLevel { get; set; }
+
         [JsonProperty("educationField")]
         public string EducationField { get; set; }
+
         [JsonProperty("companyName")]
         public string CompanyName { get; set; }
+
         [JsonProperty("jobTitle")]
         public string JobTitle { get; set; }
+
         [JsonProperty("instagramHandle")]
         public string InstagramHandle { get; set; }
+
         [JsonProperty("facebookHandle")]
         public string FacebookHandle { get; set; }
+
         [JsonProperty("twitterHandle")]
         public string TwitterHandle { get; set; }
     }
