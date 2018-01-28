11b62786d453b788adfb04582dd665da73ef2d3b
Paresh Varde <pareshvarde@yahoo.com>
2018-01-28 19:39:35 +0530

    no message

--- BK/bk-ui/src/app/main/data/categories.ts
+++ BK/bk-ui/src/app/main/data/categories.ts
@@ -12,9 +12,12 @@ export class CategoryData
 {
     public data: CategoryModel[] = [
         {id:1, category: 'Gujarati'},
-        {id:2, category: 'Marwari'},
-        {id:3, category: 'Punjabi'},
-        {id:4, category: 'Sindhi'},             
-    ];
-    
+        {id:2, category: 'Halai'},       
+        {id:3, category: 'Kachchhi'},    
+        {id:4, category: 'Maharashtrian'},       
+        {id:5, category: 'Marwari'},
+        {id:6, category: 'Punjabi'},
+        {id:7, category: 'Sindhi'},                                        
+        {id:8, category: 'Sorathia'},       
+    ];    
 }
