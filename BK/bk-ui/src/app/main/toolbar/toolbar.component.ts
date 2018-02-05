058b51e01c1e36dfa5446f697b8603cabdb5e0fe
Paresh <pareshvarde@outlook.com>
2018-02-05 19:00:06 +0530

    no message

--- BK/bk-ui/src/app/main/toolbar/toolbar.component.ts
+++ BK/bk-ui/src/app/main/toolbar/toolbar.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
-import { FuseConfigService } from '../../core/services/config.service';
-import { TranslateService } from '@ngx-translate/core';
 import { bkAuthService } from '../services/auth-service';
 import { NotificationsService } from 'angular2-notifications';
 
@@ -13,31 +11,16 @@ import { NotificationsService } from 'angular2-notifications';
 })
 
 export class FuseToolbarComponent
-{
-    userStatusOptions: any[];
-    languages: any;
-    selectedLanguage: any;
-    showLoadingBar: boolean;
-    horizontalNav: boolean;
+{    
+    languages: any;    
+    showLoadingBar: boolean;    
 
     constructor(
-        private router: Router,
-        private fuseConfig: FuseConfigService,
-        private translate: TranslateService,
+        private router: Router,                
         private alertService: NotificationsService,
         public authService: bkAuthService        
     )
-    {
-        this.languages = [
-            {
-                'id'   : 'en',
-                'title': 'English',
-                'flag' : 'us'
-            }            
-        ];
-
-        this.selectedLanguage = this.languages[0];
-
+    {       
         router.events.subscribe(
             (event) => {
                 if ( event instanceof NavigationStart )
@@ -48,29 +31,9 @@ export class FuseToolbarComponent
                 {
                     this.showLoadingBar = false;
                 }
-            });
-
-        this.fuseConfig.onSettingsChanged.subscribe((settings) => {
-            this.horizontalNav = settings.layout.navigation === 'top';
-        });
-
+            });      
     }
-
-    search(value)
-    {
-        // Do your search here...
-        console.log(value);
-    }
-
-    setLanguage(lang)
-    {
-        // Set the selected language for toolbar
-        this.selectedLanguage = lang;
-
-        // Use the selected language for translations
-        this.translate.use(lang.id);
-    }
-
+  
     logout()
     {
         let localStorage = window.localStorage;
