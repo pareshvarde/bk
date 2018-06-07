e808c28c0c6d0c2b26db6135db988875ac4cfe91
Paresh <pareshv@alohatechnology.com>
2018-06-07 12:28:59 +0530

    no message

--- BK-Data/dbo/Tables/FamilyMemberAssociation.sql
+++ BK-Data/dbo/Tables/FamilyMemberAssociation.sql
@@ -9,12 +9,15 @@
     [ApprovedBy]     INT           NULL,
     [CreatedOn]      DATETIME2 (7) CONSTRAINT [DF_FamilyMemberAssociation_CreatedOn] DEFAULT (getdate()) NOT NULL,
     [CreatedBy]      INT           NOT NULL,
+    [ModifiedOn]     DATETIME2 (7) NULL,
+    [ModifiedBy]     INT           NULL,
     CONSTRAINT [PK_FamilyMemberAssociation] PRIMARY KEY CLUSTERED ([FamilyId] ASC, [MemberId] ASC),
     CONSTRAINT [FK_FamilyMemberAssociation_Families] FOREIGN KEY ([FamilyId]) REFERENCES [dbo].[Families] ([FamilyID]),
-    CONSTRAINT [FK_FamilyMemberAssociation_lkRelationTypes] FOREIGN KEY ([RelationTypeId]) REFERENCES [dbo].[lkRelationTypes] ([RelationTypeId]),
     CONSTRAINT [FK_FamilyMemberAssociation_Members] FOREIGN KEY ([MemberId]) REFERENCES [dbo].[Members] ([MemberID]),
     CONSTRAINT [FK_FamilyMemberAssociation_Related] FOREIGN KEY ([RelatedId]) REFERENCES [dbo].[Members] ([MemberID])
 );
+
+
 
 
 
