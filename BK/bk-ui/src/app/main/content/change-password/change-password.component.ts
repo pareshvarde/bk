bb053ab911dacdf74c061410967edab00c8498e1
Paresh <pareshv@alohatechnology.com>
2018-06-07 20:55:55 +0530

    no message

--- BK/bk-ui/src/app/main/content/change-password/change-password.component.ts
+++ BK/bk-ui/src/app/main/content/change-password/change-password.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { bkDataService } from '../../services/bk-data.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Response } from '@angular/http/src/static_response';
 import { NotificationsService } from 'angular2-notifications';
 import { PasswordValidators } from 'ng2-validators'
+import  'rxjs/add/operator/takeUntil'
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.scss']  
 })
-export class ChangePasswordComponent implements OnInit {
+export class ChangePasswordComponent implements OnInit, OnDestroy {
 
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  
   changePasswordForm: FormGroup;
   formModel: changePasswordViewModel;
 
@@ -28,6 +32,11 @@ export class ChangePasswordComponent implements OnInit {
     },PasswordValidators.mismatchedPasswords('newPassword','confirmPassword'));
   }
 
+  ngOnDestroy(){
+    this.destroyed$.next(true);
+    this.destroyed$.complete(); 
+  }
+
   changePassword() {
 
     if (this.changePasswordForm.invalid)
@@ -36,9 +45,9 @@ export class ChangePasswordComponent implements OnInit {
     (<HTMLElement>document.querySelector('input[formControlName=currentPassword]')).focus();
 
     const tFormModel = (JSON.parse(JSON.stringify(this.formModel)));
-    this.changePasswordForm.reset();
+    this.changePasswordForm.reset();    
 
-    this.dataService.changePassword(tFormModel).subscribe(
+    this.dataService.changePassword(tFormModel).takeUntil(this.destroyed$).subscribe(
       (res) => {      
         this.alertService.success("Your password has been changed.");
       },
