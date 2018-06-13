bc4801a0f304d56b0911cb4d1dad1dbc0b0614e2
Paresh <pareshv@alohatechnology.com>
2018-06-13 13:48:56 +0530

    no message

--- BK/bk-ui/src/app/main/data/relations.ts
+++ BK/bk-ui/src/app/main/data/relations.ts
@@ -1,5 +1,6 @@
 export const RELATION_TYPES_DATA: any[] =
 [
+        {relationTypeId:null, relationType:'', male: true},
         {relationTypeId:1, relationType:'Son Of', male: true},
         {relationTypeId:2, relationType:'Grandson Of', male: true},        
         {relationTypeId:3, relationType:'Daughter Of', male: false},
