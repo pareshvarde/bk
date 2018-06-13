27c54392ae9f5d6f526dbb967c7c18f87b6d3a74
Paresh <pareshv@alohatechnology.com>
2018-06-13 20:26:51 +0530

    no message

---
+++ BK/Business/MemberWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace BK
+{
+    public class MemberWrapper
+    {
+        public static string ProfilePhoto(int memberId, string gender, DateTime? modifiedOn)
+        {
+            if (!modifiedOn.HasValue)
+                modifiedOn = DateTime.Now;
+
+            string filePath = System.Web.Hosting.HostingEnvironment.MapPath(string.Format(@"~/Images/Profiles/{0}.jpg", memberId));
+            if (File.Exists(filePath))
+                return string.Format("images/profiles/{0}.jpg?{1}", memberId, modifiedOn.Value.Ticks);
+
+            if (gender == "M")
+                return "images/profiles/male.png";
+            else
+                return "images/profiles/female.png";            
+        }
+    }
+}
--- BK/bk-ui/src/app/main/data/relations.ts
+++ BK/bk-ui/src/app/main/data/relations.ts
@@ -5,6 +5,8 @@ export const RELATION_TYPES_DATA: any[] =
         {relationTypeId:2, relationType:'Grandson Of', male: true},        
         {relationTypeId:3, relationType:'Daughter Of', male: false},
         {relationTypeId:4, relationType:'Granddaughter Of', male: false},        
-        {relationTypeId:5, relationType:'Wife Of', male: false}
+        {relationTypeId:5, relationType:'Wife Of', male: false},
+        {relationTypeId:6, relationType:'Father Of', male: true},
+        {relationTypeId:7, relationType:'Mother Of', male: false}
 ];    
 
