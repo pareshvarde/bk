452a7f3fc541dad78dbfb8609127296d08b89148
Paresh Varde <pareshvarde@yahoo.com>
2018-06-13 23:13:13 +0530

    no message

--- BK/bk-ui/src/app/core/components/bk-image-cropper/bk-image-cropper.component.ts
+++ BK/bk-ui/src/app/core/components/bk-image-cropper/bk-image-cropper.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
 
 @Component({
   selector: 'app-bk-image-cropper',
@@ -10,7 +11,8 @@ export class BkImageCropperComponent implements OnInit {
 
   imageChangedEvent: any = '';
   croppedImage: any = '';
-
+  @BlockUI() blockUI: NgBlockUI;
+  
   constructor(
     public dialogRef: MatDialogRef<BkImageCropperComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
@@ -29,6 +31,15 @@ export class BkImageCropperComponent implements OnInit {
     this.croppedImage = image;
   }
 
+  imageLoaded(){
+    this.blockUI.stop();
+  }
+
+  loadImageFailed(){
+    this.blockUI.stop();
+  }
+
   ngOnInit() {
+    this.blockUI.start("Please wait...");
   }
 }
