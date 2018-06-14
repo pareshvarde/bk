450c01b55a232728cca6142684966891d7ec7ecb
Paresh <pareshv@alohatechnology.com>
2018-06-14 11:56:27 +0530

    no message

--- BK/bk-ui/src/app/main/data/relations.ts
+++ BK/bk-ui/src/app/main/data/relations.ts
@@ -1,12 +1,9 @@
 export const RELATION_TYPES_DATA: any[] =
 [
         {relationTypeId:null, relationType:'', male: true},
-        {relationTypeId:1, relationType:'Son Of', male: true},
-        {relationTypeId:2, relationType:'Grandson Of', male: true},        
-        {relationTypeId:3, relationType:'Daughter Of', male: false},
-        {relationTypeId:4, relationType:'Granddaughter Of', male: false},        
+        {relationTypeId:1, relationType:'Son Of', male: true},        
+        {relationTypeId:3, relationType:'Daughter Of', male: false},        
         {relationTypeId:5, relationType:'Wife Of', male: false},
         {relationTypeId:6, relationType:'Father Of', male: true},
         {relationTypeId:7, relationType:'Mother Of', male: false}
-];    
-
+];
