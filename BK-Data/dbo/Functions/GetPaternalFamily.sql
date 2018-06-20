128448c8f63880b64acab6803d18fb5a22a2ee37
Paresh <pareshv@alohatechnology.com>
2018-06-20 12:26:05 +0530

    no message

--- BK-Data/dbo/Functions/GetPaternalFamily.sql
+++ BK-Data/dbo/Functions/GetPaternalFamily.sql
@@ -13,18 +13,19 @@ BEGIN
 	BEGIN
 		INSERT INTO @Result
 			SELECT 
-				tFamily.FamilyID, thof.FirstName + ' ' + thof.LastName FamilyName, tFamily.City + ', ' + tFamily.State FamilyAddress
+				tf.FamilyID, thof.FirstName + ' ' + thof.LastName FamilyName, tf.City + ', ' + tf.State FamilyAddress
 			FROM 
 				FamilyMemberAssociation tfma 
 				JOIN Families tf ON tf.FamilyID = tfma.FamilyId
-				JOIN Members tm on tm.MemberID = tfma.MemberID
-				JOIN Families tFamily ON tFamily.FamilyID = tfma.FamilyId
-				JOIN Members thof ON thof.MemberID = tFamily.HeadOfFamilyID
+				JOIN Members tm on tm.MemberID = tfma.MemberID				
+				JOIN Members thof ON thof.MemberID = tf.HeadOfFamilyID
+				JOIN FamilyMemberAssociation tfma1 ON tfma1.MemberId = tfma.RelatedId
 			WHERE
-				tfma.RelationTypeID = 2
+				tfma.RelationTypeID = 2				
 				AND tm.Gender = 0
 				AND tm.Married = 1
 				AND tm.MemberID = @MemberID
+				AND tfma1.DefaultFamily = 1
 	END
 
 	RETURN	
