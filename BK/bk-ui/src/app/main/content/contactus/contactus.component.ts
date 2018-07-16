e1da93441c97437df97613926f8d4f382f12f013
Paresh <pareshv@alohatechnology.com>
2018-07-16 16:53:17 +0530

    no message

--- BK/bk-ui/src/app/main/content/contactus/contactus.component.ts
+++ BK/bk-ui/src/app/main/content/contactus/contactus.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '../../../../../node_modules/@angular/forms';
+import { UniversalValidators } from '../../../../../node_modules/ng2-validators';
 
 @Component({
   selector: 'app-contactus',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactusComponent implements OnInit {
 
+  contactusForm: FormGroup;
+  
   constructor() { }
 
   ngOnInit() {
+    this.contactusForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required, UniversalValidators.isNumber]),
+      subject: new FormControl('', [Validators.required]),
+      attachment: new FormControl('', null),
+      content: new FormControl('', [Validators.required, Validators.maxLength(255)])
+    });
   }
-
 }
